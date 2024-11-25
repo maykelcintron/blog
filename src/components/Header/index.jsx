2b7092908cc6129637eb254fa1870c8c6ab16a55
Maykel Cintron <tecnopcell211000@gmail.com>
2024-11-24 22:37:18 -0400

    Updated: index.jsx img a

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -37,8 +37,10 @@ export default function Header() {
   return (
     <header id="header" className="dark:bg-[#1e2229] shadow-sm fixed top-0 left-0 w-full bg-white flex justify-center md:justify-between items-center py-4 px-10 border-b border-orange-600">
         <div className="flex items-center gap-2">
+          <a href="/">
             <img src={astroLogo ? AstroDarkLogo : AstroLogo} alt="Logo" className="w-10 h-10 cursor-pointer"/>
             <h3 id="title-header" className="dark:text-white text-2xl font-bold -mt-1">DataBD</h3>
+          </a>
         </div>
         <img onClick={handleClick} id="sun" className="bg-[#1e2229] md:p-2 p-4 rounded-full cursor-pointer fixed bottom-10 right-5 md:static border-2 border-orange-600" src={isDark ? MoonIcon : SunIcon} alt="sun" />
     </header>
